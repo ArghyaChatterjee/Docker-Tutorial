9f19b52613cffbc7b4d58ac067eb8f1b21a3557a
Arghya Chatterjee <arghyame20buet@gmail.com>
2025-05-15 19:50:35 -0500

    Update build_image.sh

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -4,6 +4,9 @@
 echo "Building Docker image 'm3t_image:latest'..."
 docker build -t m3t_image:latest . || { echo "Docker build failed"; exit 1; }
 
+# Build Docker image with logs without any cache 
+# docker build --no-cache --progress=plain -f Dockerfile.realsense -t m3t_image:0.0.2 . 2>&1 | tee docker_build.log
+
 echo "Docker image built successfully!"
 
 # Make sure the run_container.sh script is executable
